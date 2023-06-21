66b0bc06ffbd4814c4e07ecff5b62e11b4775979
aliaksah <math.uio.no>
2023-06-21 09:45:54 +0200

    changing the default alpha strategy to 3

--- R/arguments.R
+++ R/arguments.R
@@ -104,7 +104,7 @@ gen.params.gmjmcmc <- function (data) {
   ncov <- ncol(data) - 2
 
   feat_params <- list(D = 5, L = 15,                        # Hard limits on feature complexity
-                      alpha = 1,                            # alpha strategy (0 = None, 1,2,3 = strategies as per Hubin et al.) TODO: Fully Bayesian
+                      alpha = 3,                            # alpha strategy (0 = None, 1,2,3 = strategies as per Hubin et al.) TODO: Fully Bayesian
                       pop.max = as.integer(ncov * 1.5),     # Max features population size
                       keep.org = F,                         # Always keep original covariates in every population
                       prel.filter = 0,                      # Filtration threshold for first population (i.e. filter covariates even if keep.org=T)
