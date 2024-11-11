4644da59064e15ac9e666d26e1c9c575f332ca13
Ylva Sofie Tollefsen <ylvasofietollefsen@Ylva-sin-MBP.home>
2024-11-11 10:00:23 +0100

    Clean code

--- R/arguments.R
+++ R/arguments.R
@@ -125,7 +125,7 @@ gen.params.gmjmcmc <- function (data) {
 
   ncov <- ncol(data) - 2
 
-  feat_params <- list(D = 5, L = 5,                                 # Hard limits on feature complexity
+  feat_params <- list(D = 10, L = 10,                                # Hard limits on feature complexity
                       alpha = 0,                                    # alpha strategy (0 = None, 1,2,3 = strategies as per Hubin et al.) TODO: Fully Bayesian
                       pop.max = min(100,as.integer(ncov * 1.5)),    # Max features population size
                       keep.org = FALSE,                             # Always keep original covariates in every population
@@ -134,7 +134,7 @@ gen.params.gmjmcmc <- function (data) {
                       eps = 0.05,                                   # Inclusion probability limit for feature generation
                       check.col = TRUE,                             # Whether the colinearity should be checked
                       col.check.mock.data = FALSE,                  # Use mock data when checking for colinearity during feature generation
-                      max.proj.size = 15)                           # Maximum projection size
+                      max.proj.size = 10)                            # Maximum projection size
   params$feat <- feat_params
   params$rescale.large <- FALSE
   params$prel.select <- NULL                                        # Specify which covariates to keep in the first population. See Issue #15.
